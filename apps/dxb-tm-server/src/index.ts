8c8e6b5d213647042537f2e2ce653d0c74adef65
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-06-09 20:47:43 -0400

    #1 - Initial mono repo setup, initialized react app and the express server

---
+++ apps/dxb-tm-server/src/index.ts
@@ -0,0 +1,34 @@
+'use strict'
+import express, { NextFunction, Request, Response } from 'express'
+
+// Create the express app
+const app = express()
+
+// Routes and middleware
+// app.use(/* ... */)
+// app.get(/* ... */)
+
+// Error handlers
+
+
+// Start server
+app.listen(1234, function (err?: Error) {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log('Started at http://localhost:1234')
+  console.log('here;')
+})
+
+app.get('/test', (_req: Request, res: Response) => {
+  res.status(404).send('Server is running!')
+})
+
+app.use(function fourOhFourHandler(_req: Request, res: Response) {
+  res.status(404).send()
+})
+app.use(function fiveHundredHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
+  console.error(err)
+  res.status(500).send()
+})
