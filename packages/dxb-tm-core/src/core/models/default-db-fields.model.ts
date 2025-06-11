f6cfa7220a3c87ada551721c258fe365e5c0ba31
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-06-11 19:21:13 -0400

    #3 - Fixed creation issues

--- packages/dxb-tm-core/src/core/models/default-db-fields.model.ts
+++ packages/dxb-tm-core/src/core/models/default-db-fields.model.ts
@@ -3,11 +3,11 @@ import { z } from 'zod';
 export const DefaultDbFieldsSchema = z.object({
     id: z.number().int().positive('ID must be a positive number'),
     createdAt: z.date().min(new Date(0), 'Created At must be a valid date'),
-    createdBy: z.number().int().nonnegative('createdBy must be a non-negative number'),
+    createdById: z.number().int().nonnegative('createdBy must be a non-negative number'),
     updatedAt: z.date().min(new Date(0), 'Updated At must be a valid date'),
-    updatedBy: z.number().int().nonnegative('updatedBy must be a non-negative number'),
+    updatedById: z.number().int().nonnegative('updatedBy must be a non-negative number'),
     deletedAt: z.date().min(new Date(0), 'Deleted At must be a valid date').optional(),
-    deletedBy: z.number().int().nonnegative('deletedBy must be a non-negative number').optional(),
+    deletedById: z.number().int().nonnegative('deletedBy must be a non-negative number').optional(),
 });
 
 export type DefaultDbFieldsModel = z.infer<typeof DefaultDbFieldsSchema>;
