8caa67aa6a635307ddc91432e3f742d7487f36f2
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-06-19 09:30:22 -0400

    #21 - Updated the form, and form field validation using zod

--- apps/dxb-tm-client/src/app/login/page.tsx
+++ apps/dxb-tm-client/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Label } from "@radix-ui/react-label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ModeToggle } from "dxb-tm/components/mode-toggle";
 import {
     Alert,
@@ -12,46 +12,56 @@ import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "dxb-tm/components/ui/card";
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "dxb-tm/components/ui/form";
 import { Input } from "dxb-tm/components/ui/input";
 import { useAuth } from "dxb-tm/lib/hooks/use-auth";
 import type { AlertContextType } from "dxb-tm/lib/models/alert.context-type";
 import { AlertCircle } from "lucide-react";
 import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
 
 const LoginPage = () => {
     const { login } = useAuth();
+
     const [error, setError] = useState<AlertContextType | null>(null);
 
-    const handleLogin = async (e: React.FormEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const form = window.document.getElementById("login-form");
+    const LoginFormSchema = z.object({
+        email: z.string().email({ message: "Invalid email address" }),
+        password: z
+            .string()
+            .min(8, { message: "Password must be at least 8 characters" }),
+    });
 
+    const loginForm = useForm<z.infer<typeof LoginFormSchema>>({
+        resolver: zodResolver(LoginFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (formValues: z.infer<typeof LoginFormSchema>) => {
         try {
-            if (form instanceof HTMLFormElement) {
-                const formData = new FormData(form);
-
-                const email = formData.get("email") as string;
-                const password = formData.get("password") as string;
-                await login({ email, password });
-            }
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                setError({
-                    variant: "destructive",
-                    title: "Error",
-                    description: error.message,
-                });
-            } else {
-                setError({
-                    variant: "destructive",
-                    title: "Error",
-                    description: "An unknown error occurred",
-                });
-            }
+            await login(formValues);
+        } catch (error) {
+            setError({
+                variant: "destructive",
+                title: "Error",
+                description:
+                    error instanceof Error
+                        ? error.message
+                        : "An unknown error occurred",
+            });
         }
     };
 
@@ -89,50 +99,65 @@ const LoginPage = () => {
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form id="login-form">
-                        <div className="flex flex-col gap-6">
-                            <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                    name="email"
-                                />
+                    <FormProvider {...loginForm}>
+                        <form
+                            onSubmit={loginForm.handleSubmit(onSubmit)}
+                            className="space-y-8"
+                        >
+                            <FormField
+                                control={loginForm.control}
+                                name="email"
+                                render={({ field }) => {
+                                    return (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="m@example.com"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    );
+                                }}
+                            />
+                            <FormField
+                                control={loginForm.control}
+                                name="password"
+                                render={({ field }) => {
+                                    return (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="********"
+                                                    type="password"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    );
+                                }}
+                            />
+                            <div className="flex flex-col gap-2">
+                                <Button
+                                    type="submit"
+                                    className="w-full cursor-pointer"
+                                >
+                                    Login
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    className="w-full cursor-pointer"
+                                >
+                                    Sign Up
+                                </Button>
                             </div>
-                            <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    <a
-                                        href="#"
-                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
-                                </div>
-                                <Input
-                                    id="password"
-                                    type="password"
-                                    required
-                                    name="password"
-                                />
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </FormProvider>
                 </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button
-                        type="submit"
-                        className="w-full cursor-pointer"
-                        onClick={handleLogin}
-                    >
-                        Login
-                    </Button>
-                    <Button variant="outline" className="w-full cursor-pointer">
-                        Sign Up
-                    </Button>
-                </CardFooter>
             </Card>
         </div>
     );
--- apps/dxb-tm-client/src/lib/hooks/use-auth.ts
+++ apps/dxb-tm-client/src/lib/hooks/use-auth.ts
@@ -6,39 +6,41 @@ import { httpRequest } from "../utils";
 export function useAuth(): AuthContextType {
     const router = useRouter();
 
+    const login = async ({
+        email,
+        password,
+    }: {
+        email: string;
+        password: string;
+    }): Promise<void> => {
+        try {
+            const response = await httpRequest({
+                endpoint: "/auth/login",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: { email, password },
+            });
+
+            if (response.ok) {
+                router.push("/dashboard");
+            }
+
+            throw new Error(response.statusText);
+        } catch (error: unknown) {
+            throw new Error(
+                error instanceof Error
+                    ? error.message
+                    : "An unknown error occurred",
+            );
+        }
+    };
+
     return {
         isAuthenticated: false,
         isLoading: false,
-        login: async ({
-            email,
-            password,
-        }: {
-            email: string;
-            password: string;
-        }): Promise<void> => {
-            try {
-                const response = await httpRequest({
-                    endpoint: "/auth/login",
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: { email, password },
-                });
-
-                if (response.ok) {
-                    router.push("/dashboard");
-                }
-
-                throw new Error(response.statusText);
-            } catch (error: unknown) {
-                throw new Error(
-                    error instanceof Error
-                        ? error.message
-                        : "An unknown error occurred",
-                );
-            }
-        },
+        login,
         logout: () => {},
     };
 }
